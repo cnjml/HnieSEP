8dca201e70e8cd6d35a01021d3242a52f5716740
cnjml <11773052+cnjml@user.noreply.gitee.com>
2023-03-16 20:16:05 +0800

    添加验证码校验

---
+++ hniesep-base/src/main/java/com/hniesep/base/config/StringRedisTemplateConfig.java
@@ -0,0 +1,11 @@
+package com.hniesep.base.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author 吉铭炼
+ */
+@Configuration
+public class StringRedisTemplateConfig {
+
+}
