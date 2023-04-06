f4101a1bebcdf0af7d88704490923fdf9d537a1f
cnjml <11773052+cnjml@user.noreply.gitee.com>
2023-04-07 00:04:46 +0800

    文件名称更正

--- hniesep-base/src/main/java/com/hniesep/base/Generator.java
+++ hniesep-base/src/main/java/com/hniesep/base/Generator.java
@@ -1,5 +1,6 @@
 package com.hniesep.base;
 
+import com.baomidou.mybatisplus.generator.FastAutoGenerator;
 import com.baomidou.mybatisplus.generator.config.*;
 import org.springframework.stereotype.Component;
 import java.util.Collections;
@@ -14,28 +15,26 @@ public class Generator {
         String url = "jdbc:mysql://43.206.126.181:33066/base";
         String username = "root";
         String password = "Hni1sep-backend";
-        DataSourceConfig.Builder dataSourceConfigBuilder = new DataSourceConfig.Builder(url,username,password);
-        DataSourceConfig dataSourceConfig = dataSourceConfigBuilder.build();
-        //全局配置
         String author = "吉铭炼";
         String projectPath = System.getProperty("user.dir");
-        GlobalConfig globalConfig = new GlobalConfig.Builder()
-                .disableOpenDir()
-                .outputDir(projectPath+"src/main/java/")
-                .author(author)
-                .build();
-        //包配置
         String parent = "com.hniesep";
         String moduleName = "hniesep-base";
         String pathInfo = projectPath + "src/main/resources/";
-        PackageConfig packageConfig = new PackageConfig.Builder()
-                .parent(parent)
-                .moduleName(moduleName)
-                .pathInfo(Collections.singletonMap(OutputFile.xml, pathInfo))
-                .build();
-        //模板配置
-        TemplateConfig templateConfig = new TemplateConfig.Builder()
-                .build();
-
+        FastAutoGenerator.create(url, username, password)
+                .globalConfig(builder -> {
+                    builder.author(author) // 设置作者
+                            .fileOverride() // 覆盖已生成文件
+                            .outputDir(projectPath+"src/main/java/"); // 指定输出目录
+                })
+                .packageConfig(builder -> {
+                    builder.parent(parent) // 设置父包名
+                            .moduleName(moduleName) // 设置父包模块名
+                            .pathInfo(Collections.singletonMap(OutputFile.xml, pathInfo)); // 设置mapperXml生成路径
+                })
+                .strategyConfig(builder -> {
+                    builder.addInclude("t_article","t_setting","t_board","t_comment") // 设置需要生成的表名
+                            .addTablePrefix("t_"); // 设置过滤表前缀
+                })
+                .execute();
     }
 }
--- hniesep-base/src/test/java/com/hniesep/base/HnieSEPBaseApplicationTests.java
+++ hniesep-base/src/test/java/com/hniesep/base/HnieSEPBaseApplicationTests.java
@@ -1,9 +1,6 @@
 package com.hniesep.base;
 
-import com.hniesep.base.account.service.impl.RegisterServiceImpl;
-import com.hniesep.base.util.MailUtil;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
