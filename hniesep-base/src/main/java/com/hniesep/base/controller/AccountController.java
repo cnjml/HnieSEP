d42c2d3a9ca9017be1d05d7723e97f3d7a99ff9d
cnjml <11773052+cnjml@user.noreply.gitee.com>
2023-04-09 14:33:59 +0800

    新增用户名、邮箱预校验 新增spring双配置 新增用户名邮箱双校验

--- hniesep-base/src/main/java/com/hniesep/base/controller/AccountController.java
+++ hniesep-base/src/main/java/com/hniesep/base/controller/AccountController.java
@@ -56,7 +56,7 @@ public class AccountController {
      * @param password 原始表单的password
      * @return 返回类
      */
-    @RequestMapping("/originLogin")
+    @PostMapping("/originLogin")
     @ResponseBody
     @Deprecated
     public Result originLogin(@Param("username")String username,@Param("email")String email,@Param("password")String password){
@@ -70,12 +70,39 @@ public class AccountController {
      * @param user 请求体：一个json对象
      * @return 返回类
      */
-    @RequestMapping("/isRegister")
+    @PostMapping("/isExist")
     @ResponseBody
-    public Result isRegister(@RequestBody User user){
-        boolean existFlag = accountService.checkExist(user.getRegUsername(),user.getEmail());
+    public Result isExist(@RequestBody User user){
+        boolean existFlag = accountService.exist(user.getUsername(),user.getEmail());
         Integer code = existFlag ? StatusCode.EXIST_TRUE:StatusCode.EXIST_FALSE;
         String msg = existFlag ? StatusMessage.EXIST_TRUE: StatusMessage.EXIST_FALSE;
+        return new Result(code,msg);
+    }
+
+    /**
+     * 检测用户名是否注册
+     * @param user 待检测用户对象
+     * @return 用户名是否注册
+     */
+    @PostMapping("/existUsername")
+    @ResponseBody
+    public Result existUsername(@RequestBody User user){
+        boolean existFlag = accountService.existUsername(user.getUsername());
+        Integer code = existFlag ? StatusCode.EXIST_USERNAME_ONLY_TRUE:StatusCode.EXIST_USERNAME_ONLY_FALSE;
+        String msg = existFlag ? StatusMessage.EXIST_USERNAME_TRUE: StatusMessage.EXIST_USERNAME_FALSE;
+        return new Result(code,msg);
+    }
+    /**
+     * 检测邮箱是否注册
+     * @param user 待检测用户对象
+     * @return 邮箱是否注册
+     */
+    @PostMapping("/existEmail")
+    @ResponseBody
+    public Result existEmail(@RequestBody User user){
+        boolean existFlag = accountService.existEmail(user.getEmail());
+        Integer code = existFlag ? StatusCode.EXIST_EMAIL_ONLY_TRUE:StatusCode.EXIST_EMAIL_ONLY_FALSE;
+        String msg = existFlag ? StatusMessage.EXIST_EMAIL_TRUE: StatusMessage.EXIST_EMAIL_FALSE;
         return new Result(code,msg);
     }
     /**
@@ -86,29 +113,29 @@ public class AccountController {
     @PostMapping("/register")
     @ResponseBody
     public Result register(@RequestBody User user){
-        System.out.println(user.getUsername());
-        System.out.println(user.getRegPwd());
-        System.out.println(user.getEmail());
+        String username = user.getUsername();
+        String password = user.getPassword();
+        String email = user.getEmail();
         //判断验证码是否为空
-        boolean emptyVerificationCodeFlag = user.getVerificationCode()==null|| "".equals(user.getVerificationCode());
+        boolean emptyVerificationCodeFlag = user.getVerificationCode() == null || "".equals(user.getVerificationCode());
         Integer code = emptyVerificationCodeFlag ? StatusCode.VERIFICATION_CODE_EMPTY:StatusCode.VERIFICATION_CODE_EXIST;
         String msg = emptyVerificationCodeFlag ? StatusMessage.VERIFICATION_CODE_EMPTY: StatusMessage.VERIFICATION_CODE_EXIST;
         if (emptyVerificationCodeFlag){
             return new Result(code,msg);
         }
-        //判断用户名是否存在
-        boolean existFlag = accountService.checkExist(user.getUsername(),user.getEmail());
+        //判断用户名和邮箱是否存在
+        boolean existFlag = accountService.exist(username,email);
         code = existFlag ? StatusCode.EXIST_TRUE:StatusCode.EXIST_FALSE;
         msg = existFlag ? StatusMessage.EXIST_TRUE: StatusMessage.EXIST_FALSE;
-        //用户名不存在则进行下一步注册
+        //用户名和邮箱不存在则进行下一步注册
         if(!existFlag) {
             //校验验证码
-            boolean checkVerificationCodeFlag = registerService.checkRegisterVerificationCode(user.getEmail(),user.getVerificationCode());
+            boolean checkVerificationCodeFlag = registerService.checkRegisterVerificationCode(email,user.getVerificationCode());
             code = checkVerificationCodeFlag ? StatusCode.CHECK_VERIFICATION_CODE_OK : StatusCode.CHECK_VERIFICATION_CODE_ERR;
             msg = checkVerificationCodeFlag ? StatusMessage.CHECK_VERIFICATION_CODE_OK : StatusMessage.CHECK_VERIFICATION_CODE_ERR;
             //校验成功则直接注册
             if(checkVerificationCodeFlag){
-                boolean registerFlag = registerService.register(user.getEmail(),user.getUsername(),user.getRegPwd(),DateTimeUtil.getDateTime());
+                boolean registerFlag = registerService.register(email,username,password,DateTimeUtil.getDateTime());
                 code = registerFlag ? StatusCode.REGISTER_OK:StatusCode.REGISTER_ERR;
                 msg = registerFlag ? StatusMessage.REGISTER_OK:StatusMessage.REGISTER_ERR;
             }
@@ -116,28 +143,10 @@ public class AccountController {
         return new Result(code,msg);
     }
     /**
-     * 原始表单的注册
-     * @param username 原始表单的username
-     * @param password 原始表单的password
-     * @return 返回类
-     */
-    @PostMapping("/originRegister")
-    @ResponseBody
-    @Deprecated
-    public Result originRegister(@Param("username")String username,@Param("password")String password,@Param("email")String email){
-        boolean flag = accountService.checkExist(username,email);
-        Integer code = !flag ? StatusCode.REGISTER_OK:StatusCode.REGISTER_ERR;
-        String msg = !flag ? StatusMessage.REGISTER_OK: StatusMessage.REGISTER_ERR;
-        if(!flag) {
-            registerService.register(username,password,email,DateTimeUtil.getDateTime());
-        }
-        return new Result(code,msg);
-    }
-    /**
      * 获取所有用户，json格式
      * @return json对象
      */
-    @RequestMapping("/selectAll")
+    @GetMapping("/selectAll")
     @ResponseBody
     public String selectAll(){
         return accountService.selectAll().toString();
@@ -145,7 +154,7 @@ public class AccountController {
     /**
      * 获取图片验证码
      */
-    @RequestMapping("/getVerificationImage")
+    @GetMapping("/getVerificationImage")
     @ResponseBody
     public void verificationImage(HttpServletResponse httpServletResponse){
         String rightCode = VerificationUtil.generateVerificationCode();
@@ -157,7 +166,7 @@ public class AccountController {
     /**
      * 校验图片验证码
      */
-    @RequestMapping("/checkVerificationImage")
+    @PostMapping("/checkVerificationImage")
     @ResponseBody
     public boolean checkVerificationImage(@Param("code") String code, HttpSession httpSession){
         System.out.println(code);
@@ -169,7 +178,7 @@ public class AccountController {
      * @param user 用户对象
      * @return 结果
      */
-    @RequestMapping("/changePassword")
+    @PostMapping("/changePassword")
     @ResponseBody
     public Result changePassword(@RequestBody User user){
         String email = user.getEmail();
@@ -179,5 +188,21 @@ public class AccountController {
         Integer code = changePasswordFlag? StatusCode.CHANGE_PASSWORD_OK:StatusCode.CHANGE_PASSWORD_ERR;
         String msg = changePasswordFlag? StatusMessage.CHANGE_PASSWORD_OK:StatusMessage.CHANGE_PASSWORD_ERR;
         return new Result(code,msg);
+    }
+    @PostMapping("/existEmailAndUsername")
+    @ResponseBody
+    public Result existEmailAndUsername(@RequestBody User user) {
+        String email = user.getEmail();
+        String username = user.getUsername();
+        if (accountService.existUsername(username) && accountService.existEmail(email)){
+            return new Result(StatusCode.EXIST_EMAIL_AND_USERNAME_TRUE,StatusMessage.EXIST_EMAIL_AND_USERNAME_TRUE);
+        }
+        if(!accountService.existUsername(username) && !accountService.existEmail(email)) {
+            return new Result(StatusCode.EXIST_EMAIL_AND_USERNAME_FALSE,StatusMessage.EXIST_FALSE);
+        }
+        if(accountService.existUsername(username) && !accountService.existEmail(email)){
+            return new Result(StatusCode.EXIST_EMAIL_ONLY_TRUE,StatusMessage.EXIST_EMAIL_TRUE);
+        }
+        return new Result(StatusCode.EXIST_USERNAME_ONLY_TRUE,StatusMessage.EXIST_USERNAME_TRUE);
     }
 }
--- hniesep-base/src/test/java/com/hniesep/base/HnieSEPBaseApplicationTests.java
+++ hniesep-base/src/test/java/com/hniesep/base/HnieSEPBaseApplicationTests.java
@@ -1,57 +1,57 @@
-package com.hniesep.base;
-
-import com.baomidou.mybatisplus.generator.FastAutoGenerator;
-import com.baomidou.mybatisplus.generator.config.OutputFile;
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
-@SpringBootTest
-class HnieSEPBaseApplicationTests {
-    @Test
-    void generate() {
-
-        //数据源配置
-        String url = "jdbc:mysql://43.206.126.181:33066/base";
-        String username = "root";
-        String password = "Hni1sep-backend";
-        //Javadoc作者
-        String author = "吉铭炼";
-        //是否输出到main, 默认输出到test
-        boolean outToMain = false;
-        //全局输出路径配置
-        String projectPath = System.getProperty("user.dir") + "/";
-        String outPutDir = outToMain ? projectPath + "src/main/java/" : projectPath + "src/test/java/";
-        //mapper生成路径
-        String modulePath = "com/hniesep/base";
-        String pathInfo = outToMain ? projectPath + "src/main/resources/" + modulePath + "/mapper" : projectPath + "src/test/resources/" + modulePath + "/mapper";
-        //设置需要生成的表名
-        List<String> tables = new ArrayList<>();
-        tables.add("t_article");
-        tables.add("t_comment");
-        tables.add("t_setting");
-        tables.add("t_board");
-        //表前缀过滤
-        String prefix = "t_";
-        //设置父包名
-        String parent = "com.hniesep";
-        //设置子包名
-        String moduleName = "base";
-
-
-        //代码生成器
-        FastAutoGenerator.create(url, username, password)
-                .globalConfig(builder -> builder.author(author)
-                        .disableOpenDir()
-                        .outputDir(outPutDir))
-                .packageConfig(builder -> builder.parent(parent)
-                        .moduleName(moduleName)
-                        .pathInfo(Collections.singletonMap(OutputFile.xml, pathInfo)))
-                .strategyConfig(builder -> builder.addInclude(tables)
-                        .addTablePrefix(prefix))
-                .execute();
-    }
-}
+//package com.hniesep.base;
+//
+//import com.baomidou.mybatisplus.generator.FastAutoGenerator;
+//import com.baomidou.mybatisplus.generator.config.OutputFile;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//import java.util.ArrayList;
+//import java.util.Collections;
+//import java.util.List;
+//
+//@SpringBootTest
+//class HnieSEPBaseApplicationTests {
+//    @Test
+//    void generate() {
+//
+//        //数据源配置
+//        String url = "jdbc:mysql://43.206.126.181:33066/base";
+//        String username = "root";
+//        String password = "Hni1sep-backend";
+//        //Javadoc作者
+//        String author = "吉铭炼";
+//        //是否输出到main, 默认输出到test
+//        boolean outToMain = false;
+//        //全局输出路径配置
+//        String projectPath = System.getProperty("user.dir") + "/";
+//        String outPutDir = outToMain ? projectPath + "src/main/java/" : projectPath + "src/test/java/";
+//        //mapper生成路径
+//        String modulePath = "com/hniesep/base";
+//        String pathInfo = outToMain ? projectPath + "src/main/resources/" + modulePath + "/mapper" : projectPath + "src/test/resources/" + modulePath + "/mapper";
+//        //设置需要生成的表名
+//        List<String> tables = new ArrayList<>();
+//        tables.add("t_article");
+//        tables.add("t_comment");
+//        tables.add("t_setting");
+//        tables.add("t_board");
+//        //表前缀过滤
+//        String prefix = "t_";
+//        //设置父包名
+//        String parent = "com.hniesep";
+//        //设置子包名
+//        String moduleName = "base";
+//
+//
+//        //代码生成器
+//        FastAutoGenerator.create(url, username, password)
+//                .globalConfig(builder -> builder.author(author)
+//                        .disableOpenDir()
+//                        .outputDir(outPutDir))
+//                .packageConfig(builder -> builder.parent(parent)
+//                        .moduleName(moduleName)
+//                        .pathInfo(Collections.singletonMap(OutputFile.xml, pathInfo)))
+//                .strategyConfig(builder -> builder.addInclude(tables)
+//                        .addTablePrefix(prefix))
+//                .execute();
+//    }
+//}
